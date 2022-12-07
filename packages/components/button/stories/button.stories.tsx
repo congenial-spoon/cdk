1fd59f39e77af27da5fb012d53ac89c9b50251f2
TinsFox <fox@tinsfox.com>
2022-12-07 14:44:13 +0800

    feat: button

---
+++ packages/components/button/stories/button.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from '../src';
+export default {
+  title: 'Components / Button',
+  component: Button,
+};
+
+export const Basic = () => <Button  />;
--- packages/components/react/src/index.ts
+++ packages/components/react/src/index.ts
@@ -1,2 +1,3 @@
 export * from '@cuiller/rich-text';
 export * from '@cuiller/lark-auth';
+export * from '@cuiller/button';
